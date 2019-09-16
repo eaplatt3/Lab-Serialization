c0e0d40ad04d03d2e0e464092ed86efb5201ef15
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-09-16 10:09:55 -0400

    Build first DataContract

--- Lab-Serialization/Program.cs
+++ Lab-Serialization/Program.cs
@@ -3,13 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
 
 namespace Lab_Serialization
 {
+    [DataContract]
+    class Address
+    {
+        string city;
+        string state;
+
+        public Address()
+        {
+
+        }
+
+        [DataMember(Name = "city")]
+        public string City
+        {
+            get
+            {
+                return city;
+            }
+
+            set
+            {
+                city = value;
+            }
+        }
+
+        [DataMember(Name = "state")]
+        public string State
+        {
+            get
+            {
+                return state;
+            }
+
+            set
+            {
+                state = value;
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Address add = new Address();
+
+            String filename = "address.json";
+            add.City = "Bronx";
+            add.State = "New York";
+
+            FileStream writer = new FileStream(filename, FileMode.Create, FileAccess.Write);
+
+            DataContractJsonSerializer ser;
+            ser = new DataContractJsonSerializer(typeof(Address));
+
+            ser.WriteObject(writer, add);
+            writer.Close();
+
+            Address add2;
+            String filename2 = "address.jason";
+
+            FileStream reader = new FileStream(filename2, FileMode.Open, FileAccess.Read);
+
+            DataContractJsonSerializer inputSerializer;
+            inputSerializer = new DataContractJsonSerializer(typeof(Address));
+
+            add2 = (Address)inputSerializer.ReadObject(reader);
+            reader.Close();
         }
     }
 }
